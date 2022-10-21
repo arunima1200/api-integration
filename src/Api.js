9bcc1c7b0d99f628b873f795b7fcfb45ee73e390
arunima1200 <arunima2829@gmail.com>
2022-10-21 19:27:41 +0530

    First commit

---
+++ src/Api.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Api = () => {
+    const [post, setPost] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://random-data-api.com/api/users/random_user?size=10").then((response) => {
+            console.log(response.data);
+            setPost(response.data);
+        });
+
+    }, [])
+
+    const handleRandomChange = () => {
+        window.location.reload(false);
+    }
+
+    return(
+        <div>
+            <div className="container">
+                { post && post.map((val) => {
+                    return(
+                        <div className="content">
+                        
+                            <div className="front">
+                            <li>
+                                <img src={val.avatar} alt="Avatar"/>
+                            </li>
+                            </div>
+                        
+                        
+                            <div className="back">
+                            <li>
+                                {val.first_name} {val.last_name}
+                            </li>
+                            </div>
+                        
+                        </div>
+                    )
+                })}
+            </div>  
+            <button className="btn" onClick={handleRandomChange}>Click Here</button>
+        </div>
+    )
+}
+
+export default Api;
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Api from './Api';
+import './Api.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>API Data Cards</h1>
+        <Api/>
+    </div>
+  );
+}
+
+export default App;
